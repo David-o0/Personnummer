81782cbdf5375b036ad125a922617070bcf2e54f
00oo00 <_o0@10.11.12.128>
2016-11-20 17:42:44 +0100

    Första försöket på att dela upp i main och annnan class

---
+++ Personnummer161120-1716/src/personnummer161120/pkg1716/KorrektPersonNummer.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personnummer161120.pkg1716;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class KorrektPersonNummer {
+
+	static Date dagsdato = new Date();
+	static Calendar kalender = new GregorianCalendar();
+	static Scanner scan = new Scanner(System.in);
+
+	private int year;
+	private int month;
+	private int day;
+	private int yearCalendar = Calendar.YEAR;
+        
+// Constructors 
+    public KorrektPersonNummer() {
+    }
+
+    public KorrektPersonNummer(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+        
+        
+        // Getters
+	public int getYearCalendar() { return yearCalendar; }
+        public int getYear() { return year;  }
+        public int getMonth(){ return month; }
+	public int getDay() {  return day;   }
+
+        // Setters
+        public void setYear(int year)   { this.year = year; }
+	public void setMonth(int month) { this.month = month; }
+        public void setDay(int day)     { this.day = day; }
+
+// här fanns main i en provversion ///////////////////////////
+        
+        public static boolean nummerValidering(String persNr) {
+
+        int numberSize = persNr.length();
+
+        if (numberSize != 10) {
+                System.out.println("Personnummer är störe eller mindre än 10 siffror!");
+                return false;
+
+        } else if (persNr.matches("\\d+")) {
+                return true;
+        } else {
+                System.out.println("Personnummer får inte innehålla bokstav! ");
+                return false;
+        }
+
+}
+	
+
+	public static boolean datumValidering(String persNr) { 
+
+		persNr = "19" + persNr;
+		KorrektPersonNummer kpn = new KorrektPersonNummer();
+
+		kalender.setTime(dagsdato);
+		kalender.setLenient(false);
+
+		kpn.setYear(Integer.parseInt(persNr.substring(0, 4)));
+		kpn.setMonth(Integer.parseInt(persNr.substring(4, 6)));
+		kpn.setDay(Integer.parseInt(persNr.substring(6, 8)));
+		
+
+		// Compare the year if 1900 or 2000...
+		if (kpn.getYear() >= 1947 && kpn.getYear() <= 1999) {
+			// do nothing..
+		} else if (kpn.getYear() >= 1900 && kpn.getYear() <= kpn.getYearCalendar()) {
+			kpn.setYear(kpn.getYear() + 100);
+
+		} else {
+			kpn.setYear(kpn.getYear() + 100);
+		}
+		
+
+		if (kpn.getYear() > kalender.get(kpn.getYearCalendar())) {
+
+			System.out.println("Personnummer tillhör en ofödd!");
+			System.out.println("Födelse år: " + kpn.getYear() + " / nuvarande år: "
+					+ kalender.get(kpn.getYearCalendar()));
+			//printDatum(kpn.getYear(), kpn.getMonth(), kpn.getDay());
+			return false;
+
+		} else {
+			if ((kpn.getMonth() <= 12 && kpn.getMonth() > 0) && (kpn.getDay() > 0 && kpn.getDay() <= 31)) {
+								
+				//printDatum(kpn.getYear(), kpn.getMonth(), kpn.getDay());
+				return true;
+
+			} else {
+				//printDatum(kpn.getYear(), kpn.getMonth(), kpn.getDay());
+				System.out.println("Månad eller dagen stämmer inte! Försök igen..");
+				return false;
+			}
+
+		}
+
+	}
+
+	public static void printDatum(int year, int month, int day) {
+
+		System.out.println("Year: " + year);
+		System.out.println("Month: " + month);
+		System.out.println("Day: " + day);
+	}
+
+	
+
+	public static void checkNumber(String pn) { 
+
+		int lastNumber = Integer.parseInt(pn.substring(9, 10).toString());
+		pn = pn.substring(0, 9).toString();
+
+		int summa = 0;
+		int summa2 = 0;
+		int[] testa = new int[10];
+		
+		
+		for (int i = 0; i < 9; i++) {
+			int number = Integer.parseInt(pn.substring(i, i +1));
+			
+			if((i % 2)!= 0){
+				summa2 = number;
+				
+				if(summa2 > 9){
+					testa[i]= (1 + (summa2 % 10));
+				}else {
+					testa[i] = summa2;
+				}
+				
+			}else {
+				summa = number *2;
+				
+				if(summa > 9){
+					testa[i] = (1+ (summa %10));
+				}else{
+					testa[i] = summa;
+				}
+			}
+			
+		}		
+		
+//		for (int j = 0; j < (testa.length) - 1; j++) {
+//			System.out.println(testa[j]);
+//		}
+		
+		int total = testa[0] + testa[1] + testa[2] + testa[3] + testa[4] + testa[5] + testa[6] + testa[7] +
+					testa[8] + testa[9];
+		
+		total = total % 10;
+		
+		if((10 - total) == lastNumber){
+			System.out.println("> Personnummer är giltigt!");
+		}else {
+			System.out.println("> Personnummer är ogiltigt!");
+		}
+	}
+
+	
+
+	
+
+}
---
+++ Personnummer161120-1716/src/personnummer161120/pkg1716/Personnummer1611201716.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personnummer161120.pkg1716;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author _o0
+ */
+public class Personnummer1611201716 {
+
+    /**
+     * @param args the command line arguments
+     */
+    	public static void main(String[] args) {
+
+		while (true) {
+
+			System.out.println("Mata in ditt personnummer 10 siffror 'ÅÅMMDD-xxxx' eller 'ÅÅMMDDxxxx");
+			Scanner scan = new Scanner(System.in);
+                        String personNummer = scan.nextLine();
+			personNummer = personNummer.replaceAll("-", "").trim();
+
+			if (KorrektPersonNummer.nummerValidering(personNummer)) {
+				if (KorrektPersonNummer.datumValidering(personNummer)) {
+					KorrektPersonNummer.checkNumber(personNummer);
+					break;
+				}
+
+			}
+
+		}
+
+	}
+}
